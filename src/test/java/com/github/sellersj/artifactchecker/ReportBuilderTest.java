91c5e90fe7344619125173e2aa7682280e15d8d3
Jim Sellers <jim.sellers@canada.ca>
2017-12-14 16:44:28 -0500

    fixing env var access

--- src/test/java/com/github/sellersj/artifactchecker/ReportBuilderTest.java
+++ src/test/java/com/github/sellersj/artifactchecker/ReportBuilderTest.java
@@ -9,8 +9,8 @@ public class ReportBuilderTest {
 
     @Test
     public void buildJsonReport() {
-        String toolsHost = System.getProperty("TOOLS_HOST");
-        assertTrue("The TOOLS_HOST variable has to be set", StringUtils.isBlank(toolsHost));
+        String toolsHost = System.getenv("TOOLS_HOST");
+        assertTrue("The TOOLS_HOST variable has to be set", StringUtils.isNotBlank(toolsHost));
 
         String location = "https://" + toolsHost + "/deployed-to/manifest-combined.txt";
         String target = "/data00/bamboo/projectsites/app-inventory.json";
