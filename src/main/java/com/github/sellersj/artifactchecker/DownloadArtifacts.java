c18fb35d46e769e6718c69de5ebea214726a9053
Jim Sellers <jim.sellers@canada.ca>
2018-10-31 15:13:33 -0400

    bumping up owasp check version

--- src/main/java/com/github/sellersj/artifactchecker/DownloadArtifacts.java
+++ src/main/java/com/github/sellersj/artifactchecker/DownloadArtifacts.java
@@ -26,8 +26,8 @@ import com.github.sellersj.artifactchecker.model.owasp.DependencyCheck;
 import com.github.sellersj.artifactchecker.model.owasp.Vulnerability;
 
 /**
- * If doing this on a computer that hasn't updated the owasp dependency check data, it be can be
- * done by calling the <code>org.owasp:dependency-check-maven:3.0.2:update-only</code>
+ * If doing this on a computer that hasn't updated the owasp dependency check data, it be can be done by calling the
+ * <code>org.owasp:dependency-check-maven:3.0.2:update-only</code>
  *
  * @author sellersj
  *
@@ -35,7 +35,7 @@ import com.github.sellersj.artifactchecker.model.owasp.Vulnerability;
 public class DownloadArtifacts {
 
     /** The version of owasp dependency check to use. */
-    private static final String OWASP_DEP_CHECK_VERSION = "3.3.2";
+    private static final String OWASP_DEP_CHECK_VERSION = "3.3.4";
 
     /** The version of maven-dependency-plugin to use. */
     private static final String MAVEN_DEP_PLUGIN_VERSION = "3.1.1";
@@ -173,8 +173,8 @@ public class DownloadArtifacts {
     }
 
     /**
-     * For improperly deployed apps, the snapshots might not exist in the repo any more. So we're
-     * doing a maven install to be able to do the CVE checks.
+     * For improperly deployed apps, the snapshots might not exist in the repo any more. So we're doing a maven install
+     * to be able to do the CVE checks.
      *
      * @param gav to use
      * @param projectDir the directory that it's in
@@ -252,8 +252,8 @@ public class DownloadArtifacts {
     }
 
     /**
-     * try to get a list of the tags, see if we have 1 unique version that ends with the version,
-     * and then try to switch to that.
+     * try to get a list of the tags, see if we have 1 unique version that ends with the version, and then try to switch
+     * to that.
      *
      * @param gav to switch to
      * @param projectDir where the project is already cloned to
