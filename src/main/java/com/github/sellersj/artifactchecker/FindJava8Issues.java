3af73c88b876969f107009b4958ffe8a876971cd
Jim Sellers <jim.sellers@canada.ca>
2019-12-17 15:51:48 -0500

    fixing regex

--- src/main/java/com/github/sellersj/artifactchecker/FindJava8Issues.java
+++ src/main/java/com/github/sellersj/artifactchecker/FindJava8Issues.java
@@ -55,7 +55,7 @@ public class FindJava8Issues {
         "org.eclipse.jdt.core.*:3.10.0|" + //
         "org.springframework:spring-.*:3.2.9|" + //
         "org.springframework:spring-.*:3.2.1[0-9]|" + //
-        "org.springframework:spring-.*:[4-9|" + //
+        "org.springframework:spring-.*:[4-9]|" + //
         ":provided|" + //
         ":test" + //
         // make sure above line doesn't end with a pipe character |
