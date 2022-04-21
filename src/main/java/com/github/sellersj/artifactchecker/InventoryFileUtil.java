3d211984a5a790f4453896a4780eb1dd5d868737
Jim Sellers <jim.sellers@ised-isde.gc.ca>
2022-04-21 12:57:18 -0400

    default a tech owner if the scm projcet is CWP

--- src/main/java/com/github/sellersj/artifactchecker/InventoryFileUtil.java
+++ src/main/java/com/github/sellersj/artifactchecker/InventoryFileUtil.java
@@ -249,6 +249,8 @@ public class InventoryFileUtil {
                 if (StringUtils.isNotBlank(owner.getJiraKey())) {
                     System.out.println("Updating TechOwner for " + app.getJiraKey() + " to " + owner.getTechOwner());
                     app.setTechOwner(owner.getTechOwner());
+                } else if ("CWP".equals(app.getScmProject())) {
+                    app.setTechOwner("CIPO");
                 }
             }
         }
