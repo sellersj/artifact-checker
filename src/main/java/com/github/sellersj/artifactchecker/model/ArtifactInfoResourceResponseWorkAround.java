62e1a7e62972fbac473b2174761921e70256fd5c
Jim Sellers <jim.sellers@ised-isde.gc.ca>
2024-10-28 15:28:58 -0400

    working on moving to java 21 #287

--- src/main/java/com/github/sellersj/artifactchecker/model/ArtifactInfoResourceResponseWorkAround.java
+++ src/main/java/com/github/sellersj/artifactchecker/model/ArtifactInfoResourceResponseWorkAround.java
@@ -2,6 +2,10 @@ package com.github.sellersj.artifactchecker.model;
 
 import org.sonatype.nexus.rest.model.ArtifactInfoResourceResponse;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
 /**
  *
  * This is a work around since the original class sets the top level names which do not show up in the response that I
@@ -10,8 +14,8 @@ import org.sonatype.nexus.rest.model.ArtifactInfoResourceResponse;
  * We are using the class name instead of "artifact-info"
  */
 @SuppressWarnings("all")
-@javax.xml.bind.annotation.XmlRootElement(name = "org.sonatype.nexus.rest.model.ArtifactInfoResourceResponse")
-@javax.xml.bind.annotation.XmlAccessorType(javax.xml.bind.annotation.XmlAccessType.FIELD)
+@XmlRootElement(name = "org.sonatype.nexus.rest.model.ArtifactInfoResourceResponse")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class ArtifactInfoResourceResponseWorkAround extends ArtifactInfoResourceResponse {
 
 }
